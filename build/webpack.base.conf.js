04b7cd06e7d15a8ab9d63bfecbd0872081198a56
Alexander Beach <ajbeach2@gmail.com>
2016-03-20 11:01:20 -0500

    bad gh page path

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, '../dist/static'),
-    publicPath: '/static/',
+    publicPath: '/vue-scroll-view/static/',
     filename: '[name].js'
   },
   resolve: {
